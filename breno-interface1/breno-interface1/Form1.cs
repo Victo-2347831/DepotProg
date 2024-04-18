151f114080d92e6322668b7ff75dcbed7fcb19ec
BrenoGombre <brebr7no@gmail.com>
2024-04-18 10:56:41 -0400

    form1-breno

--- breno-interface1/breno-interface1/Form1.cs
+++ breno-interface1/breno-interface1/Form1.cs
@@ -1,8 +1,8 @@
 namespace breno_interface1
 {
-    public partial class Form1 : Form
+    public partial class FormTitre : Form
     {
-        public Form1()
+        public FormTitre()
         {
             InitializeComponent();
         }
